0260df1a04415f6ba5353728bbddf44583d6b75d
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-09 13:23:32 +0300

    moviessl

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const fs = require("fs");
 const PORT = 5000;
 app.use(express.json());
 
-
+//ممكن تكون كذا او تكون جوا كل راوت
+//let movies = [];
+//fs.readFile('./movies.json', (err, data) => {
+//const movies = JSON.parse(data.toString());
+//});
 
 function addFile(movies) {
 fs.writeFile("./movies.json", JSON.stringify(movies),() => {
@@ -33,27 +37,30 @@ const {name} = req.body;
     let movies = JSON.parse(data.toString());
 
   movies.push ({ 
-      id: movies.length, 
+      id: movies.length +1 , 
       name: name, 
       isFav: false, 
       isDeleted: false 
     });
   
       addFile(movies)
-      res.status(200).json(movies);
+      res.status(201).json(movies);
 });
 })
 ///////////////////////////////////////////////////////////
 
-// 4 - update by id
-app.put('/update/:id/',(req,res)=> {
+// 4 - update all by id
+app.put('/update/:id',(req,res)=> {
     fs.readFile("./movies.json", (err,data) => {
         let movies = JSON.parse(data.toString());
 
-        movies.map((elem) => {
-            elem
+        const { name, isDeleted, isFav } = req.body // all togther
+        movies.forEach((elem) => {
         if(elem.id == req.params.id)  {
-        elem.name = req.body.name;
+        if(name != undefined) elem.name = name;
+        if(isDeleted != undefined) elem.isDeleted = isDeleted;
+        if(isFav != undefined) elem.isFav = isFav;
+
         }
     })
     addFile(movies);
